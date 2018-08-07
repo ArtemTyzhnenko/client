43f1ca3bb0e6770ec4cfbcf9e169f5b3d54cc67f
Artem Tyzhnenko <artem.tyznhenko@chisw.com>
2018-08-07 22:19:45 +0300

    create signup component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 class Header extends Component {
-
-
     render() {
         return(
             <div>
                 <Link to='/'>Redux Auth</Link>
-                <Link to='/'>Sign Up</Link>
-                <Link to='/'>Sign In</Link>
-                <Link to='/'>Sign Out</Link>
-                <Link to='/'>Feature</Link>
+                <Link to='/signup'>Sign Up</Link>
+                <Link to='/signin'>Sign In</Link>
+                <Link to='/signout'>Sign Out</Link>
+                <Link to='/feature'>Feature</Link>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from  'react-router-dom'
 
 import App from './components/App';
-import Welcome from './components/Welcome/Welcome'
+import Welcome from './components/Welcome/Welcome';
+import Signup from './components/Auth/Signup/Signup';
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
             <Route path="/" exact component={Welcome}/>
+            <Route path="/signup" component={Signup}/>
         </App>
     </BrowserRouter>
     , document.getElementById('root'));
