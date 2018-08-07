41dd6e317ad811b881f713d35136be773a7f59cf
Artem Tyzhnenko <artem.tyznhenko@chisw.com>
2018-08-07 22:12:03 +0300

    create header(navigation) and 'welcome' component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+class Header extends Component {
+
+
+    render() {
+        return(
+            <div>
+                <Link to='/'>Redux Auth</Link>
+                <Link to='/'>Sign Up</Link>
+                <Link to='/'>Sign In</Link>
+                <Link to='/'>Sign Out</Link>
+                <Link to='/'>Feature</Link>
+            </div>
+        )
+    }
+}
+
+export default Header;
